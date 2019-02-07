34101e0943615957b47ae0271eff1142280da56f
Nicolas <Nicolas.Pla@corbisstudio.com>
2019-02-07 09:56:08 -0300

    Navigation Drawer completo

--- app/src/main/java/com/example/seccion09/Activities/MainActivity.java
+++ app/src/main/java/com/example/seccion09/Activities/MainActivity.java
@@ -1,5 +1,8 @@
 package com.example.seccion09.Activities;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
@@ -7,12 +10,18 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Gravity;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
+import com.example.seccion09.Fragments.AlertsFragment;
+import com.example.seccion09.Fragments.EmailFragment;
+import com.example.seccion09.Fragments.InfoFragment;
 import com.example.seccion09.R;
 
 public class MainActivity extends AppCompatActivity {
 
     private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,7 +30,75 @@ public class MainActivity extends AppCompatActivity {
         setToolBar();
 
         drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.navview);
 
+        setFragmentByDefault();
+
+        drawerLayout.addDrawerListener(new DrawerLayout.DrawerListener() {
+            @Override
+            public void onDrawerSlide(@NonNull View view, float v) {
+                
+            }
+
+            @Override
+            public void onDrawerOpened(@NonNull View view) {
+                Toast.makeText(MainActivity.this, "Opened", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onDrawerClosed(@NonNull View view) {
+                Toast.makeText(MainActivity.this, "Closed", Toast.LENGTH_SHORT).show();
+
+            }
+
+            @Override
+            public void onDrawerStateChanged(int i) {
+
+            }
+        });
+
+        // Botones del navview
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+                boolean fragmentTransaction = false;
+                Fragment fragment = null;
+
+                switch (menuItem.getItemId()){
+                    case R.id.menu_mail:
+                        fragment = new EmailFragment();
+                        fragmentTransaction = true;
+                        break;
+
+                    case  R.id.menu_alert:
+                        fragment = new AlertsFragment();
+                        fragmentTransaction = true;
+                        break;
+
+                    case  R.id.menu_info:
+                        fragment = new InfoFragment();
+                        fragmentTransaction = true;
+
+
+                    case  R.id.menu_opcion_1:
+                        Toast.makeText(MainActivity.this, "Opcion 1", Toast.LENGTH_SHORT).show();
+                    break;
+
+
+                    case  R.id.menu_opcion_2:
+                        Toast.makeText(MainActivity.this, "Opcion 2", Toast.LENGTH_SHORT).show();
+                        break;
+                }
+
+                if (fragmentTransaction){
+                    changeFragment(fragment, menuItem);
+                    drawerLayout.closeDrawers(); // Cierra el menu
+                }
+
+                return true ;
+            }
+        });
     }
     private void setToolBar(){
         Toolbar toolbar = findViewById(R.id.toolbar);
@@ -30,6 +107,20 @@ public class MainActivity extends AppCompatActivity {
         // Icono de menu
         getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_home);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    private void setFragmentByDefault(){
+        changeFragment(new EmailFragment(), navigationView.getMenu().getItem(0));
+    }
+
+    private void changeFragment(Fragment fragment, MenuItem item) {
+
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.content_frame, fragment)
+                .commit();
+        item.setChecked(true);
+        getSupportActionBar().setTitle(item.getTitle());
     }
 
     @Override
